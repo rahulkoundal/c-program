add74974b55f4b10d7b42cae897daac270d25d95
rahulkoundal1996@gmail.com <root@localhost.localdomain>
2018-06-24 15:42:44 +0530

    odd number program

---
+++ odd.c
@@ -0,0 +1,27 @@
+//program for odd number
+//odd number 1,3,5,7....
+//kali linux vim editior programming
+//printf() and scanf() function use
+//if else concept
+
+
+#include<stdio.h>
+int main()
+{
+	int a; //decleration of the variable
+
+	//user input through the keyboard
+	printf("enter the value of A=");
+	scanf("%d",&a);
+
+	//if else concept
+	if(a%2!=0)
+	{
+		printf("number is odd number\n");
+	}
+	else
+	{
+		printf("number is not odd\n");
+	}
+	return 1151;
+}
